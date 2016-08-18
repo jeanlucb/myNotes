c84722753a3b7ad124b7f22a13c7a3f4b1f3f0fd
Jean-Luc Barras <jeanluc.barras@gmail.com>
2016-08-18 06:31:50 +0200

    Restricted visibility of documents on S3.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,4 +9,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
   }
   config.fog_directory = ENV["AWS_S3_BUCKET"]
+  config.fog_public    = false
+  config.fog_authenticated_url_expiration = 5*60
 end
